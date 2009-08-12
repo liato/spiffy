396ade07c3d609061937312edd2f9b6183dc5370
liato <x@x00.us>
2009-08-13 01:07:47 +0200

    Initial commit.

---
+++ bot.py
@@ -0,0 +1,486 @@
+import time, imp, os, sys, re, threading
+from twisted.words.protocols import irc
+from twisted.words.protocols.irc import lowDequote, numeric_to_symbolic, symbolic_to_numeric
+from twisted.internet import reactor, protocol
+#from twisted.python import log
+
+def parsemsg(s):
+    """Breaks a message from an IRC server into its prefix, command, arguments and text.
+    """
+    prefix = None
+    text = None
+    if not s:
+        raise irc.IRCBadMessage("Empty line.")
+    if s[0] == ':':
+        prefix, s = s[1:].split(' ', 1)
+    if s.find(' :') != -1:
+        s, text = s.split(' :', 1)
+
+    args = s.split()
+    command = args.pop(0)
+    return prefix, command, args, text
+
+def sourcesplit(source):
+    """Split nick!user@host and return a 3-value tuple."""
+    r = re.compile(r'([^!]*)!?([^@]*)@?(.*)')
+    m = r.match(source)
+    return m.groups()
+
+class Bot(irc.IRCClient): 
+
+    def connectionMade(self):
+        config = self.factory.config
+        connections = self.factory.connections
+        connections[config['network']] = self
+  
+        self.nickname = config.get('nick', 'spiffy')
+        self.username = config.get('user', self.nickname)
+        self.realname = config.get('name', self.nickname)
+        self.versionName = config.get('versionreply', None)
+  
+        self.channels = config.get('channels', [])
+        self.stack = []
+        self.config = config
+        self.connections = connections
+        self.verbose = True
+        self.encoding = 'utf-8'
+        self.userlist = UserList(self)
+        
+
+        print "Loading modules..."
+        self.variables = {}
+        self.doc = {}
+  
+        filenames = []
+        for fn in os.listdir(os.path.join(sys.path[0], 'modules')): 
+           if fn.endswith('.py') and not fn.startswith('_'): 
+              filenames.append(os.path.join(sys.path[0], 'modules', fn))
+
+        modules = []
+        for filename in filenames: 
+           name = os.path.basename(filename)[:-3]
+           try: module = imp.load_source(name, filename)
+           except Exception, e: 
+              print >> sys.stderr, "Error loading %s: %s (in bot.py)" % (name, e)
+           else: 
+              if hasattr(module, 'setup'): 
+                 module.setup(self)
+              #print "registering %s" % name
+              self.register_module(vars(module))
+              modules.append(name)
+  
+        if modules: 
+           print >> sys.stderr, 'Registered modules:', ', '.join(modules)
+        else: print >> sys.stderr, "Warning: Couldn't find any modules"
+        
+        self.bind_commands()
+        
+        irc.IRCClient.connectionMade(self)
+        print "connected at %s" % time.asctime(time.localtime(time.time()))
+        
+    def register_module(self, variables): 
+        # This is used by reload.py, hence it being methodised
+        for name, obj in variables.iteritems(): 
+            if hasattr(obj, 'commands') or hasattr(obj, 'rule'): 
+                self.variables[name] = obj
+
+    def bind_commands(self): 
+        self.commands = {}
+        
+        def bind(self, priority, regexp, func): 
+            print priority, regexp.pattern.encode('utf-8'), func
+            # register documentation
+            if not hasattr(func, 'name'): 
+                func.name = func.__name__
+            if func.__doc__: 
+                if hasattr(func, 'example'): 
+                    example = func.example
+                    example = example.replace('$nickname', self.nickname)
+                else: example = None
+                self.doc[func.name] = (func.__doc__, example)
+            self.commands.setdefault(regexp, []).append(func)
+  
+        def sub(pattern, self=self): 
+            # These replacements have significant order
+            pattern = pattern.replace('$nickname', self.nickname)
+            return pattern.replace('$nick', r'%s[,:] +' % self.nickname)
+  
+        for name, func in self.variables.iteritems(): 
+            # print name, func
+            if not hasattr(func, 'priority'): 
+                func.priority = 'medium'
+   
+            if not hasattr(func, 'thread'): 
+                func.thread = True
+   
+            if not hasattr(func, 'event'): 
+                func.event = 'PRIVMSG'
+            else:
+                func.event = func.event.upper()
+   
+            if hasattr(func, 'rule'): 
+                if isinstance(func.rule, str): 
+                    pattern = sub(func.rule)
+                    regexp = re.compile(pattern)
+                    bind(self, func.priority, regexp, func)
+    
+                if isinstance(func.rule, tuple): 
+                    # 1) e.g. ('$nick', '(.*)')
+                    if len(func.rule) == 2 and isinstance(func.rule[0], str): 
+                        prefix, pattern = func.rule
+                        prefix = sub(prefix)
+                        regexp = re.compile(prefix + pattern, re.IGNORECASE)
+                        bind(self, func.priority, regexp, func)
+     
+                    # 2) e.g. (['p', 'q'], '(.*)')
+                    elif len(func.rule) == 2 and isinstance(func.rule[0], list): 
+                        prefix = self.config['prefix']
+                        commands, pattern = func.rule
+                        for command in commands:
+                            #command = r'(%s)(?: +(?:%s))?' % (command, pattern)
+                            #command = r'(%s)|(%s)(?: +(?:%s))' % (command,command, pattern)
+                            command = r'(%s)(?: +(?:%s))?$' % (command, pattern)
+                            regexp = re.compile(prefix + command, re.IGNORECASE)
+                            bind(self, func.priority, regexp, func)
+     
+                    # 3) e.g. ('$nick', ['p', 'q'], '(.*)')
+                    elif len(func.rule) == 3: 
+                        prefix, commands, pattern = func.rule
+                        prefix = sub(prefix)
+                        for command in commands: 
+                            command = r'(%s) +' % command
+                            regexp = re.compile(prefix + command + pattern, re.IGNORECASE)
+                            bind(self, func.priority, regexp, func)
+   
+            if hasattr(func, 'commands'): 
+                for command in func.commands: 
+                    template = r'^%s(%s)(?: +(.*))?$'
+                    pattern = template % (self.config['prefix'], command)
+                    regexp = re.compile(pattern, re.IGNORECASE)
+                    bind(self, func.priority, regexp, func)        
+        
+    def connectionLost(self, reason):
+        irc.IRCClient.connectionLost(self, reason)
+        print "disconnected at %s" % time.asctime(time.localtime(time.time()))
+
+    # callbacks for events
+
+    def signedOn(self):
+        """Called when bot has succesfully signed on to server."""
+        for chan in self.channels:
+            self.join(chan)
+
+    def joined(self, channel):
+        """This will get called when the bot joins the channel."""
+        print "[I have joined %s]" % channel
+
+    def privmsg(self, user, channel, msg):
+        """This will get called when the bot receives a message."""
+          
+        if msg.startswith(".cmd"):
+                cmd = msg.split()[1:]
+                cmd = " ".join(cmd)
+                msg = ""
+                try:
+                    res = eval(cmd)
+                    if res != None:
+                        msg = chr(2) + "Result: " + chr(2) + str(res)
+                except Exception, e:
+                    msg = chr(2) + "EXCEPTION! >> " + chr(2) + str(e)
+                if msg:
+                    self.msg(channel, msg)
+
+    def sendLine(self, line):
+        if self.encoding is not None:
+            if isinstance(line, unicode):
+                line = line.encode(self.encoding)
+        self.transport.write("%s%s%s" % (line, chr(015), chr(012)))
+        
+
+    def lineReceived(self, line):
+        line = lowDequote(line)
+        try:
+            prefix, command, params, text = parsemsg(line)
+            if numeric_to_symbolic.has_key(command):
+                command = numeric_to_symbolic[command]
+            self.handleCommand(command, prefix, params, text, line)
+        except Exception, e:
+            print "Error: %s" % e
+        
+
+        
+    def handleCommand(self, command, prefix, params, text, line):
+        """Determine the function to call for the given command and call
+        it with the given arguments.
+        """
+        method = getattr(self, "irc_%s" % command, None)
+        #print command, prefix, params
+        if method is not None:
+            try:
+                method(prefix or '', text and params+[text] or params)
+            except:
+                pass
+
+        command = (symbolic_to_numeric.get(command, command), command)
+        if command[0].upper() in ("JOIN", "352", "353", "KICK", "PART", "QUIT", "NICK", "MODE"):
+            self.userlist._handleChange(prefix, command, params, text)
+        if command[0] == "005":
+            self.sendLine('PROTOCTL NAMESX') 
+            
+        print line
+        print prefix, command, params, text
+        print "\n"
+        items = self.commands.items()
+        #print "items: %s" % len(items)
+        for regexp, funcs in items: 
+            for func in funcs:
+                #print "func.event: %s; event: %s" % (func.event, event)
+                if not func.event in command:
+                    continue
+
+                match = regexp.match(text)
+                if match:
+                    print "hej1"
+                    input = CommandInput(self, prefix, command, params, text, match, line)
+                    print "hej2"
+                    bot = QuickReplyWrapper(self, input)
+                    print "hej3"
+                    #self.call(func, bot, input)
+                    targs = (func, bot, input)
+                    t = threading.Thread(target=self.call, args=targs)
+                    t.start()
+
+    def call(self, func, bot, input):
+        #try:
+            func(bot, input)
+        #except Exception, e:
+        #    print "Error: %s" % str(e)
+        #    if self.config.get('chandebug', True):
+        #        self.msg(input.sender, str(e))
+            #self.error(input)
+
+
+
+    def ctcpQuery_VERSION(self, user, channel, data):
+        nick = user.split("!",1)[0]
+        if self.versionName:
+            self.ctcpMakeReply(nick, [('VERSION', '%s' % self.versionName)])
+
+class CommandInput(object):
+    def __init__(self, bot, source, command, params, text, match, line):
+        self.nick, self.user, self.host = sourcesplit(source or '')
+        self.line = line
+        self.command = command
+        self.match = match
+        self.group = match.group
+        self.groups = match.groups
+        self.args = params
+        if len(params) > 0: 
+            self.sender = params[0]
+        else:
+            self.sender = None
+
+        mappings = {bot.nickname: self.nick, None: None}
+        self.sender = mappings.get(self.sender, self.sender)
+        self.channel = self.sender
+        self._bot = bot
+        
+    def isowner(self, *args):
+        if not 'ownermask' in self._bot.config:
+            return False
+        if re.search(self._bot.config['ownermask'], self.nick+'!'+self.user+'@'+self.host, re.IGNORECASE):
+            return True
+        return False        
+            
+class QuickReplyWrapper(object): 
+    def __init__(self, bot, input): 
+        self.bot = bot
+        self.input = input
+
+    def __getattribute__(self, attr):
+        try:
+            return object.__getattribute__(self, attr)
+        except AttributeError:
+            return getattr(object.__getattribute__(self, 'bot'), attr)
+        
+    def reply(self, msg):
+        if self.input.sender:
+            self.bot.msg(self.input.sender, self.input.nick + ': ' + msg)
+    
+    def say(self, msg):
+        if self.input.sender:
+            self.bot.msg(self.input.sender, msg)
+
+
+class UserList(object):
+    def __init__(self, bot):
+        self.channels = {}
+        self.bot = bot
+        
+    def __getitem__(self, attr, default=None):
+        return self.channels[attr]
+        
+    def _handleChange(self, prefix, command, params, text):
+        command = command[0]
+        nick, user, host = sourcesplit(prefix or '')
+        chan = text or params[0]
+        chan = chan.lower()
+        
+        #JOIN
+        if command.lower() == "join":
+            #Clear the channels userlist and issue a who command when the bot joins a chan
+            if nick == self.bot.nickname:
+                self.channels[chan] = {}
+                self.bot.sendLine("WHO %s" % chan)
+                self.bot.sendLine("NAMES %s" % chan)
+
+            self.channels[chan][nick.lower()] = {'nick': nick, 'user': user, 'host': host, 'mode': None}
+
+        #RPL_WHOREPLY
+        elif command == "352":
+            chan = params[1].lower()
+            nick = params[5]
+            user = params[2]
+            host = params[3]
+        
+            if not chan in self.channels:
+                self.channels[chan] = {}
+            
+            self.channels[chan][nick.lower()] = {'nick': nick, 'user': user, 'host': host, 'mode': None}
+            
+        #RPL_NAMREPLY
+        elif command == "353":
+            chan = params[2].lower()
+            nicks = text.lower().split()
+            if chan in self.channels:
+                exp = re.compile(r'(?P<mode>[~%&@+]*)(?P<nick>[^~%&@+]*)')
+                for nick in nicks:
+                    m = exp.match(nick)
+                    if m.group('nick') in self.channels[chan]:
+                        self.channels[chan][m.group('nick')]['mode'] = m.group('mode')
+
+        #KICK
+        elif command.lower() == "kick":
+            chan = input.rawline.split(' ')[2].lower()
+            nick = params[1].lower()
+            if chan in self.channels:
+                if nick == self.bot.nickname.lower():
+                    #Remove channel from userlist when the bot is kicked from the channel.
+                    del self.self.channels[chan]
+                elif nick in self.channels[chan]:
+                    del self.channels[chan][nick]
+
+        #PART
+        elif command.lower() == "part":
+            chan = params[0].lower()
+            nick = nick.lower()
+            if chan in self.channels:
+                if nick == self.bot.nickname.lower():
+                    #Remove channel from userlist when the bot is kicked from the channel.
+                    del self.self.channels[chan]
+                elif nick in self.channels[chan]:
+                    del self.channels[chan][nick]                    
+
+        #QUIT
+        elif command.lower() == "quit":
+            nick = nick.lower()
+            for chan in self.channels:
+                if nick in self.channels[chan]:
+                    del self.channels[chan][nick]
+                
+        #NICK
+        elif command.lower() == "nick":
+            nick = nick.lower()
+            newnick = params[0]
+            for chan in self.channels:
+                if nick in self.channels[chan]:
+                    self.channels[chan][newnick.lower()] = self.channels[chan][nick]
+                    self.channels[chan][newnick.lower()]['nick'] = newnick
+                    if nick != newnick.lower():
+                        del self.channels[chan][nick] 
+
+        #MODE
+        elif command.lower() == "mode":
+            mode = params[1]
+            chan = params[0].lower()
+            if 'o' or 'v' in mode:
+                self.bot.sendLine('NAMES %s' % chan)
+
+    #Is <nick> on <chan>?
+    def ison(self, nick, chan):
+        nick = nick.lower()
+        chan = chan.lower()
+        if chan in self.channels:
+            if nick in self.channels[chan]:
+                return True
+        return False
+    
+    #Returns <nick>'s attributes on <chan>
+    #Valid attributes are: 'nick', 'user', 'host', 'mode' (or 'all' for a dict with all attributes)
+    def uinfo(self, nick, chan, attr):
+        nick = nick.lower()
+        chan = chan.lower()
+        if self.ison(nick, chan):
+            if attr == 'all':
+                return self.channels[chan][nick]
+            elif attr in ('nick', 'user', 'host', 'mode'):
+                return self.channels[chan][nick][attr]
+        return None
+    
+    #Returns <nick>'s modes on <chan>
+    def getmode(self, nick, chan):
+        nick = nick.lower()
+        chan = chan.lower()
+        if self.ison(nick, chan):
+            return self.channels[chan][nick]['mode']
+        return None
+    
+    
+    #Is <nick> op'd on <chan>?
+    def isop(self, nick, chan):
+        if self.getmode(nick, chan):
+            if '@' in self.getmode(nick, chan):
+                return True
+        return False
+    
+    
+    #Is <nick> voiced on <chan>?
+    def isvoice(self, nick, chan):
+        if self.getmode(nick, chan):
+            if '+' in self.getmode(nick, chan):
+                return True
+        return False
+    
+    
+    #Is <nick> a regular user on <chan>?
+    def isreg(self, nick, chan):
+        if self.getmode(nick, chan) == None:
+            return True
+        return False
+    
+    
+    #Return a list of channels <nick> is on
+    def chans(self, nick):
+        chans = []
+        nick = nick.lower()
+        for chan in self.channels:
+            if nick in self.channels[chan]:
+                chans.append(chan)
+        return chans                
+
+
+class BotFactory(protocol.ClientFactory):
+     # the class of the protocol to build when new connection is made
+    protocol = Bot
+
+    def __init__(self, config, connections):
+        self.config = config
+        self.connections = connections
+
+    def clientConnectionLost(self, connector, reason):
+        """If we get disconnected, reconnect to server."""
+        connector.connect()
+
+    def clientConnectionFailed(self, connector, reason):
+        print "connection failed:", reason
+        reactor.stop()
---
+++ spiffy.py
@@ -0,0 +1,54 @@
+import sys, os, imp, random
+from bot import BotFactory
+from twisted.internet import reactor
+
+connections = {}
+if __name__ == '__main__':
+    if sys.version_info < (2, 4): 
+        print >> sys.stderr, 'Error: Requires Python 2.4 or later, from www.python.org'
+        sys.exit(1)    
+
+    config_name = os.path.join(sys.path[0], 'config.py')
+    if not os.path.isfile(config_name):
+        print >> sys.stderr, 'Error: No config(.py) file found.'
+        sys.exit(1)
+
+    config = imp.load_source('config', config_name)
+    config.filename = config_name
+    
+    if hasattr(config, 'networks'):
+        for network in config.networks:
+            #serverconfig = config.networks[network]
+            #serverconfig = config.__dict__.copy()
+            serverconfig = {}
+            config_defaults = {'prefix': r'\.', 'nick': 'spiffy', 'chandebug': True, 'channels': []}
+            for x in config_defaults:
+                serverconfig[x] = config_defaults[x]
+            for x in config.__dict__:
+                if not x.startswith('__'):
+                    serverconfig[x] = config.__dict__[x]
+            for x in config.networks[network]:
+                serverconfig[x] = config.networks[network][x]
+                
+            serverconfig['network'] = network
+
+            if 'host' not in serverconfig:
+                print 'Error: Could not connect to %s. No host given.' % network
+            else:
+                print 'Creating connection to %s' % network
+                server = serverconfig['host']
+                port = 6667
+                if isinstance(server, (tuple, list)):
+                    server = server[random.randrange(len(server))]
+                
+                if ':' in server:
+                    server, port = server.split(':')
+                    port = int(port)
+
+                serverconfig['activeserver'] = '%s:%s' % (server, port or 6667)
+                reactor.connectTCP(server, port or 6667, BotFactory(serverconfig, connections))
+        reactor.run()
+
+    else:
+        print >> sys.stderr, "Error: No netorks to connect to."
+        sys.exit(1)
