e27f1a1d1567302038f304d2444da11fce79c2bc
liato <x@x00.us>
2010-08-21 00:36:37 +0200

    Fixed whatis plugin.

--- plugins/whatis.py
+++ plugins/whatis.py
@@ -2,60 +2,45 @@ import re
 import urllib
 import urllib2
 
+from utils import decodehtml, removehtml
 qurl = "http://www.google.com/search?q=%s&hl=en"
 
 def whatis(self, input):
     """Performs a "what is <argument>" query to Google and displays the result"""
 
-    msg = lambda message: self.say(message)
-    
     if not input.args:
         raise self.BadInputError()
 
     query = input.args.strip()
 
     showurl = False
-    if "-u " in query:
+    if query.startswith("-u "):
         showurl = True
-    query = "what is " + query.replace("-u ","")
+        query = query[3:]
+    query = "what is " + query
     
     query = query.encode('utf-8')
     url = qurl % urllib.quote(query)
     
     if showurl:
-        msg(chr(2) + "URL: " + chr(2) + url)
+        self.say(chr(2) + "URL: " + chr(2) + url)
 
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor)
     headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.2) Gecko/2008091620 Firefox/3.0.2',
                'Connection':'Keep-Alive', 'Content-Type':'application/x-www-form-urlencoded'}
     
     page = opener.open(urllib2.Request(url,None,headers)).read()
-    gsays = chr(2) + "Google says: " + chr(2)
-
-    match = re.search(r"<td nowrap[^>]*><h2 class=r [^>]+><b>(.+?)</b>", page,re.IGNORECASE)
-    if match:
-        result = match.group(1).strip().replace("&quot;","\"")
-        result = re.sub(r"<[^>]+?>","",result)
-        msg(gsays+result)
-        return
-    del match
     
-    match = re.search("Web definitions for <b>.+?<td valign=to[^>]+>([^<]+)<br>", page)
-    if match:
-        msg(gsays+match.group(1).strip().replace("&quot;","\""))
-        return
-    del match
+    regexes = [r"<h[23]\sclass=r[^>]*><b>(.+?)</b></h[23]>",
+             r"onebox/[^>]*>(.*?)<(?:/table|br)"]
     
-    match = re.search(r'alt="Clock"></td><td valign=[^>]+><b>([^<]+)</b>([^\-]+)- <b>',page)
-    if match:
-        time = match.group(1).strip()
-        zone = match.group(2).strip()
-        result = time + ", " + zone
+    for regex in regexes:
+        match = re.search(regex, page,re.IGNORECASE)
+        if match:
+            self.say(decodehtml(removehtml(match.group(1).strip())))
+            return
 
-        msg(gsays + result)
-        return
-
-    msg("Google knows not!")
+    self.say("Dunno :S")
 
 whatis.rule = ["whatis", "w"]
 whatis.usage = [("Use Google Calculator to convert between currencies", "$pcmd <amount> <currency> in <amount> <currency>"),
